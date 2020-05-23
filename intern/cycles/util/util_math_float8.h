c95fdc321565a889585c92542df8602791d995f2
pembem22 <24252798+pembem22@users.noreply.github.com>
2020-05-23 19:13:12 +0300

    Optimize float8 operations using AVX

--- intern/cycles/util/util_math_float8.h
+++ intern/cycles/util/util_math_float8.h
@@ -93,8 +93,12 @@ ccl_device_inline bool isequal(const float8 a, const float8 b);
 #ifndef __KERNEL_OPENCL__
 ccl_device_inline float8 operator+(const float8 &a, const float8 &b)
 {
+#  ifdef __KERNEL_AVX2__
+  return float8(_mm256_add_ps(a.m256, b.m256));
+#  else
   return make_float8(
       a.a + b.a, a.b + b.b, a.c + b.c, a.d + b.d, a.e + b.e, a.f + b.f, a.g + b.g, a.h + b.h);
+#  endif
 }
 
 ccl_device_inline float8 operator+(const float8 &a, const float f)
@@ -109,13 +113,22 @@ ccl_device_inline float8 operator+(const float f, const float8 &a)
 
 ccl_device_inline float8 operator-(const float8 &a)
 {
+#  ifdef __KERNEL_AVX2__
+  __m256 mask = _mm256_castsi256_ps(_mm256_set1_epi32(0x80000000));
+  return float8(_mm256_xor_ps(a.m256, mask));
+#  else
   return make_float8(-a.a, -a.b, -a.c, -a.d, -a.e, -a.f, -a.g, -a.h);
+#  endif
 }
 
 ccl_device_inline float8 operator-(const float8 &a, const float8 &b)
 {
+#  ifdef __KERNEL_AVX2__
+  return float8(_mm256_sub_ps(a.m256, b.m256));
+#  else
   return make_float8(
       a.a - b.a, a.b - b.b, a.c - b.c, a.d - b.d, a.e - b.e, a.f - b.f, a.g - b.g, a.h - b.h);
+#  endif
 }
 
 ccl_device_inline float8 operator-(const float8 &a, const float f)
@@ -130,8 +143,12 @@ ccl_device_inline float8 operator-(const float f, const float8 &a)
 
 ccl_device_inline float8 operator*(const float8 &a, const float8 &b)
 {
+#  ifdef __KERNEL_AVX2__
+  return float8(_mm256_mul_ps(a.m256, b.m256));
+#  else
   return make_float8(
       a.a * b.a, a.b * b.b, a.c * b.c, a.d * b.d, a.e * b.e, a.f * b.f, a.g * b.g, a.h * b.h);
+#  endif
 }
 
 ccl_device_inline float8 operator*(const float8 &a, const float f)
@@ -146,8 +163,12 @@ ccl_device_inline float8 operator*(const float f, const float8 &a)
 
 ccl_device_inline float8 operator/(const float8 &a, const float8 &b)
 {
+#  ifdef __KERNEL_AVX2__
+  return float8(_mm256_div_ps(a.m256, b.m256));
+#  else
   return make_float8(
       a.a / b.a, a.b / b.b, a.c / b.c, a.d / b.d, a.e / b.e, a.f / b.f, a.g / b.g, a.h / b.h);
+#  endif
 }
 
 ccl_device_inline float8 operator/(const float8 &a, const float f)
@@ -187,12 +208,21 @@ ccl_device_inline float8 operator/=(float8 &a, float f)
 
 ccl_device_inline bool operator==(const float8 &a, const float8 &b)
 {
+#  ifdef __KERNEL_AVX2__
+  return (_mm256_movemask_ps(_mm256_castsi256_ps(
+              _mm256_cmpeq_epi32(_mm256_castps_si256(a.m256), _mm256_castps_si256(b.m256)))) &
+          0b11111111) == 0b11111111;
+#  else
   return (a.a == b.a && a.b == b.b && a.c == b.c && a.d == b.d && a.e == b.e && a.f == b.f &&
           a.g == b.g && a.h == b.h);
+#  endif
 }
 
 ccl_device_inline float8 rcp(const float8 &a)
 {
+#  ifdef __KERNEL_AVX2__
+  return float8(_mm256_rcp_ps(a.m256));
+#  else
   return make_float8(1.0f / a.a,
                      1.0f / a.b,
                      1.0f / a.c,
@@ -201,6 +231,7 @@ ccl_device_inline float8 rcp(const float8 &a)
                      1.0f / a.f,
                      1.0f / a.g,
                      1.0f / a.h);
+#  endif
 }
 
 ccl_device_inline float8 safe_rcp(const float8 &a)
@@ -217,6 +248,9 @@ ccl_device_inline float8 safe_rcp(const float8 &a)
 
 ccl_device_inline float8 sqrt(const float8 &a)
 {
+#  ifdef __KERNEL_AVX2__
+  return float8(_mm256_sqrt_ps(a.m256));
+#  else
   return make_float8(sqrtf(a.a),
                      sqrtf(a.b),
                      sqrtf(a.c),
@@ -225,6 +259,7 @@ ccl_device_inline float8 sqrt(const float8 &a)
                      sqrtf(a.f),
                      sqrtf(a.g),
                      sqrtf(a.h));
+#  endif
 }
 
 ccl_device_inline float8 sqr(const float8 &a)
@@ -234,8 +269,7 @@ ccl_device_inline float8 sqr(const float8 &a)
 
 ccl_device_inline bool is_zero(const float8 &a)
 {
-  return (a.a == 0.0f && a.b == 0.0f && a.c == 0.0f && a.d == 0.0f && a.e == 0.0f && a.f == 0.0f &&
-          a.g == 0.0f && a.h == 0.0f);
+  return a == make_float8(0.0f);
 }
 
 ccl_device_inline float average(const float8 &a)
@@ -245,6 +279,9 @@ ccl_device_inline float average(const float8 &a)
 
 ccl_device_inline float8 min(const float8 &a, const float8 &b)
 {
+#  ifdef __KERNEL_AVX2__
+  return float8(_mm256_min_ps(a.m256, b.m256));
+#  else
   return make_float8(min(a.a, b.a),
                      min(a.b, b.b),
                      min(a.c, b.c),
@@ -253,10 +290,14 @@ ccl_device_inline float8 min(const float8 &a, const float8 &b)
                      min(a.f, b.f),
                      min(a.g, b.g),
                      min(a.h, b.h));
+#  endif
 }
 
 ccl_device_inline float8 max(const float8 &a, const float8 &b)
 {
+#  ifdef __KERNEL_AVX2__
+  return float8(_mm256_max_ps(a.m256, b.m256));
+#  else
   return make_float8(max(a.a, b.a),
                      max(a.b, b.b),
                      max(a.c, b.c),
@@ -265,6 +306,7 @@ ccl_device_inline float8 max(const float8 &a, const float8 &b)
                      max(a.f, b.f),
                      max(a.g, b.g),
                      max(a.h, b.h));
+#  endif
 }
 
 ccl_device_inline float8 clamp(const float8 &a, const float8 &mn, const float8 &mx)
@@ -274,6 +316,9 @@ ccl_device_inline float8 clamp(const float8 &a, const float8 &mn, const float8 &
 
 ccl_device_inline float8 fabs(const float8 &a)
 {
+#  ifdef __KERNEL_AVX2__
+  return float8(_mm256_and_ps(a.m256, _mm256_castsi256_ps(_mm256_set1_epi32(0x7fffffff))));
+#  else
   return make_float8(fabsf(a.a),
                      fabsf(a.b),
                      fabsf(a.c),
@@ -282,6 +327,7 @@ ccl_device_inline float8 fabs(const float8 &a)
                      fabsf(a.f),
                      fabsf(a.g),
                      fabsf(a.h));
+#  endif
 }
 
 ccl_device_inline float8 mix(const float8 &a, const float8 &b, float t)
@@ -305,7 +351,13 @@ ccl_device_inline float8 reduce_max(const float8 &a)
 
 ccl_device_inline float8 reduce_add(const float8 &a)
 {
+#  ifdef __KERNEL_AVX2__
+  float8 b(_mm256_hadd_ps(a.m256, a.m256));
+  float8 h(_mm256_hadd_ps(b.m256, b.m256));
+  return make_float8(h[0] + h[4]);
+#  else
   return make_float8(a.a + a.b + a.c + a.d + a.e + a.f + a.g + a.h);
+#  endif
 }
 
 ccl_device_inline float reduce_min_f(const float8 &a)
@@ -364,22 +416,15 @@ ccl_device_inline float8 safe_divide_even(const float8 a, const float8 b)
 
 ccl_device_inline float8 ensure_finite(float8 v)
 {
-  if (!isfinite_safe(v.a))
-    v.a = 0.0f;
-  if (!isfinite_safe(v.b))
-    v.b = 0.0f;
-  if (!isfinite_safe(v.c))
-    v.c = 0.0f;
-  if (!isfinite_safe(v.d))
-    v.d = 0.0f;
-  if (!isfinite_safe(v.e))
-    v.e = 0.0f;
-  if (!isfinite_safe(v.f))
-    v.f = 0.0f;
-  if (!isfinite_safe(v.g))
-    v.g = 0.0f;
-  if (!isfinite_safe(v.h))
-    v.h = 0.0f;
+  v.a = ensure_finite(v.a);
+  v.b = ensure_finite(v.b);
+  v.c = ensure_finite(v.c);
+  v.d = ensure_finite(v.d);
+  v.e = ensure_finite(v.e);
+  v.f = ensure_finite(v.f);
+  v.g = ensure_finite(v.g);
+  v.h = ensure_finite(v.h);
+
   return v;
 }
 
@@ -391,6 +436,9 @@ ccl_device_inline bool isfinite_safe(float8 v)
 
 ccl_device_inline float8 pow(float8 v, float e)
 {
+#ifdef __KERNEL_AVX2__
+  return float8(_mm256_pow_ps(v.m256, _mm256_set1_ps(e)));
+#else
   return make_float8(powf(v.a, e),
                      powf(v.b, e),
                      powf(v.c, e),
@@ -399,24 +447,54 @@ ccl_device_inline float8 pow(float8 v, float e)
                      powf(v.f, e),
                      powf(v.g, e),
                      powf(v.h, e));
+#endif
 }
 
 ccl_device_inline float8 exp(float8 v)
 {
+#ifdef __KERNEL_AVX2__
+  return float8(_mm256_exp_ps(v.m256));
+#else
   return make_float8(
       expf(v.a), expf(v.b), expf(v.c), expf(v.d), expf(v.e), expf(v.f), expf(v.g), expf(v.h));
+#endif
+}
+
+ccl_device_inline float8 expm1(float8 v)
+{
+#ifdef __KERNEL_AVX2__
+  return float8(_mm256_expm1_ps(v.m256));
+#else
+  return make_float8(expm1f(v.a),
+                     expm1f(v.b),
+                     expm1f(v.c),
+                     expm1f(v.d),
+                     expm1f(v.e),
+                     expm1f(v.f),
+                     expm1f(v.g),
+                     expm1f(v.h));
+#endif
 }
 
 ccl_device_inline float8 log(float8 v)
 {
+#ifdef __KERNEL_AVX2__
+  return float8(_mm256_log_ps(v.m256));
+#else
   return make_float8(
       logf(v.a), logf(v.b), logf(v.c), logf(v.d), logf(v.e), logf(v.f), logf(v.g), logf(v.h));
+#endif
 }
 
 ccl_device_inline float dot(const float8 &a, const float8 &b)
 {
+#ifdef __KERNEL_AVX2__
+  float8 t(_mm256_dp_ps(a.m256, b.m256, 0xFF));
+  return t[0] + t[4];
+#else
   return (a.a * b.a) + (a.b * b.b) + (a.c * b.c) + (a.d * b.d) + (a.e * b.e) + (a.f * b.f) +
          (a.g * b.g) + (a.h * b.h);
+#endif
 }
 
 CCL_NAMESPACE_END
